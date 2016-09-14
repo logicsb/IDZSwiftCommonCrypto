a3887fcc7e11c656b2ddc9670db42a5684bc2606
Breno Lima <breno@behive.io>
2016-09-14 03:29:30 -0300

    no message

--- GenerateCommonCryptoModule.swift
+++ GenerateCommonCryptoModule.swift
@@ -70,7 +70,7 @@ enum SDK: String {
     
 }
 
-guard let sdk = SDK(rawValue: CommandLine.arguments[1])?.rawValue else { reportError(message:"SDK must be one of \(SDK.all.map { $0.rawValue })") }
+guard let sdk = SDK(rawValue: CommandLine.arguments[0])?.rawValue else { reportError(message:"SDK must be one of \(SDK.all.map { $0.rawValue })") }
 guard let sdkVersion = trim(s:runShellCommand(command:"/usr/bin/xcrun --sdk \(sdk) --show-sdk-version")) else {
     reportError(message:"ERROR: Failed to determine SDK version for \(sdk)")
 }
@@ -86,8 +86,8 @@ if verbose {
 
 let moduleDirectory: String
 let moduleFileName: String
-if CommandLine.arguments.count > 2 {
-    moduleDirectory =  "\(CommandLine.arguments[2])/Frameworks/\(sdk)/CommonCrypto.framework"
+if CommandLine.arguments.count > 1 {
+    moduleDirectory =  "\(CommandLine.arguments[1])/Frameworks/\(sdk)/CommonCrypto.framework"
     moduleFileName = "module.map"
 }
 else {
